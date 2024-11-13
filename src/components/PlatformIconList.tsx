8daefaf5318c13e84d313f8ac8a0db9c419927f8
imt-eiaz <imteiaz@gmail.com>
2024-11-13 08:51:21 +0000

    icon failed to load

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,44 @@
-import { Platform } from '../hooks/useGames'
-import { Text } from '@chakra-ui/react'
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
 
 interface Props {
-    platforms: Platform[]
+  platforms: Platform[];
 }
 
- const PlatformIconList = ({platforms} : Props) => {
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    //name : PlayStation
+    // Slug :
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    andriod: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} />
+      ))}
+    </HStack>
+  );
+};
 
-     return (
-        <>
-        {platforms.map((platform) => <Text>{platform.name}</Text>)}
-        </>
-    )
-    }
-    
-export default PlatformIconList
+export default PlatformIconList;
