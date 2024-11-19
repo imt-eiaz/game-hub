3962056597f6fe6faa0a73239795fe3c2ce87f5e
imt-eiaz <imteiaz@gmail.com>
2024-11-19 19:04:16 +0000

    half way to searchinput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,22 +1,30 @@
 import { InputGroup } from "./ui/input-group";
 import { LuSearch } from "react-icons/lu";
 
-import { HStack, Input, Kbd } from "@chakra-ui/react";
-import { NativeSelectField, NativeSelectRoot } from "./ui/native-select";
+import { HStack, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 
 const SearchInput = () => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
     <>
-      <HStack width="full" borderRadius={20}>
-        <InputGroup
-          flex="1"
-          startElement={<LuSearch />}
-          //endElement={<Kbd></Kbd>}
-        >
-          <Input placeholder="Search games..." />
-        </InputGroup>
-      </HStack>
-      <label> Themes </label>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (ref.current) console.log(ref.current.value);
+        }}
+      >
+        <HStack width="full" borderRadius={20}>
+          <InputGroup
+            flex="1"
+            startElement={<LuSearch />}
+            //endElement={<Kbd></Kbd>}
+          >
+            <Input ref={ref} placeholder="Search games..." />
+          </InputGroup>
+        </HStack>
+      </form>
     </>
   );
 };
